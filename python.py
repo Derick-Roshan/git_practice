4e5da3a9de5fbf8e8180700be439b6041097e531
Roshan <roshanrebel23@gmail.com>
2026-07-10 18:53:25 +0530

    this commit is for sample

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-sum = num1 + num2 #this line is empty
+sum = num1 + num2 # Noe this is edited with VS Code!
 
 print("Sum =", sum)
